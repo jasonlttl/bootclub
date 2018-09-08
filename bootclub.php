51ffc832736351b06b1133e90ab0683c7a2f926d
Jason Little <little.129@osu.edu>
2018-09-08 19:02:17 -0400

    adds spells, attacks, saves.

--- bootclub.php
+++ bootclub.php
@@ -11,18 +11,112 @@ foreach (glob('./xml/*.xml') as $file) {
 
   $abilities = abilify($xml);
   $skills = skillify($xml);
+  $spells = spellify($xml);
+  $actions = actions($xml);
+  $saves = saves($xml);
 
   file_put_contents($out, $twig->render('character.twig', [
     'character' => $xml->character,
     'skills' => $skills,
     'abilities' => $abilities,
+    'spells' => $spells,
+    'actions' => $actions,
+    'saves' => $saves,
   ]));
 }
 
+function bab($xml) {
+  $bab = 0;
+  foreach ($xml->character->class as $class) {
+    $bab += (int) $class->baseAttack;
+  }
+  return $bab;
+}
+
+function saves($xml) {
+  $able = abilify($xml);
+  $saves = [
+    'fortitude' => 0,
+    'reflex' => 0,
+    'will' => 0,
+  ];
+  foreach ($xml->character->class as $class) {
+    foreach (array_keys($saves) as $save) {
+      $saves[$save] += (int) $class->$save;
+    }
+  }
+  $saves['fortitude'] += $able['CON']['bonus'];
+  $saves['reflex'] += $able['DEX']['bonus'];
+  $saves['will'] += $able['WIS']['bonus'];
+  return $saves;
+}
+
+function actions($xml) {
+  $able = abilify($xml);
+  $bab = bab($xml);
+  $map = [
+    '%1' => $able['STR']['bonus'],
+    '%2' => $able['DEX']['bonus'],
+    '%3' => $able['CON']['bonus'],
+    '%4' => $able['INT']['bonus'],
+    '%5' => $able['WIS']['bonus'],
+    '%6' => $able['CHA']['bonus'],
+    '%8' => $bab,
+    '%9' => $bab + $able['STR']['bonus'],
+    '%10' => $bab + $able['DEX']['bonus'],
+  ];
+
+  $actions = [];
+  foreach ($xml->character->action as $action) {
+    $attacks = [];
+    foreach ($action->attack as $attack) {
+      foreach ($map as $key=>$val){
+        $attack = str_replace($key, $val, $attack);
+      }
+      print $attack;
+      $attack = eval("return {$attack};");
+      $attacks[] = "+" . $attack;
+    }
+    $name = (string)$action->name;
+    $actions[$name] = [
+      'attacks' => $attacks,
+      'damage' => (string) $action->damage,
+    ];
+  }
+  return $actions;
+}
+
+function spellify($xml) {
+  $spells = [];
+  foreach ($xml->character->class as $class) {
+    if ($class->spell) {
+      $class_name = (string) $class->name;
+      $spells[$class_name] = [];
+      foreach ($class->spell as $spell) {
+        $level = $spell->level ? (string) $spell->level : '0';
+        if (!isset($spells[$class_name][$level])) {
+          $spells[$class_name][$level] = [];
+        }
+        $spells[$class_name][$level][(string) $spell->name] = $spell;
+      }
+    }
+  }
+
+  // Sort everything
+  ksort($spells);
+  foreach (array_keys($spells) as $class) {
+    ksort($spells[$class]);
+    foreach (array_keys($spells[$class]) as $level) {
+      ksort($spells[$class][$level]);
+    }
+  }
+  //print_r($spells);
+  return $spells;
+}
 
 function abilify($xml) {
   $abilities = explode(',', $xml->character->abilities);
-  print_r($abilities);
+  //print_r($abilities);
   $vals = [];
   foreach (['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA'] as $ability) {
     $vals[$ability] = [
@@ -48,6 +142,8 @@ function abilityMap($in) {
 function bonusify($raw) {
   return floor($raw / 2) - 5;
 }
+
+
 
 function skillify($xml) {
   $abilities = abilify($xml);
